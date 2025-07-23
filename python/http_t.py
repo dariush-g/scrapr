57089582eef1b6c929deb60562ecd7dc50af2605
Dariush Ghassemieh <dariush@ghassemieh.com>
2025-07-22 22:55:58 -0700

    reformat

--- python/http_t.py
+++ python/http_t.py
@@ -1,10 +1,8 @@
 import scrapr
 
-# smoke-test the raw HTTP fetch
 html = scrapr.fetch_http("example.com", "/")
 assert "Example Domain" in html
 
-# test your tag extractor
 html = "<p>Hello</p><div>skip</div><p>World</p>"
 paras = scrapr.extract_tag(html, "p")
 assert paras == ["Hello", "World"]
